a41110b893319983eba75920bb17d2f658a3df40
Adelson Saguate <adelsonsaguate@acsun-mz.com>
2025-01-09 19:35:04 +0200

    Adicionado o react icons e ajustes nos icons

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,8 +1,15 @@
+import { FaFacebook, FaLinkedin, FaInstagram } from "react-icons/fa";
+import styles from './Footer.module.css'
 function Footer() {
     const currentYear = new Date().getFullYear(); // Obtém o ano atual
 
     return (
         <footer>
+            <ul className={styles.social_list}>
+                <li ><FaFacebook/></li>
+                <li><FaLinkedin/></li>
+                <li><FaInstagram/></li>
+            </ul>
             <p>Todos os direitos reservados &copy; {currentYear}</p>
         </footer>
     );

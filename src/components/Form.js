c6db4b367343aeb63b1bed3497c7da0192f0bacb
Adelson Saguate <adelsonsaguate@acsun-mz.com>
2025-01-09 15:14:17 +0200

    Aplicando o conceito do useState na prática

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,20 +1,39 @@
+import {useState} from 'react'
 function Form() {
-
     function addUser(e){
         e.preventDefault();
-        const nome = document.querySelector('input[name="nome"]').value;
-        alert(`Nome cadastrado: ${nome}`);
-        document.querySelector('input[name="nome"]').value = '';
-        document.querySelector('form').reset();
+        alert("O utilizador: " + nome+" foi registrado, com a senha: " + senha);
+        
     }
+
+    const [nome,setNome] = useState();
+    const [senha,setSenha] = useState();
     
     return (
         <div>
             <h1>Dados Pessoais</h1>
             <form onSubmit={addUser}>
                 <div>
-                    <label>Nome</label>
-                    <input type="text" placeholder="Digite o seu nome" name="nome"/>
+                    <label htmlFor="nome">Nome: </label>
+                    <input 
+                        type="text" 
+                        name="nome" 
+                        id="nome" 
+                        placeholder="Digite o seu nome" 
+                        onChange={(e) => setNome(e.target.value)}
+                        
+                    />
+                </div>
+                <div>
+                    <label htmlFor="senha">Senha: </label>
+                    <input 
+                        type="password" 
+                        name="senha" 
+                        id="senha" 
+                        placeholder="Digite a sua senha"
+                        onChange={(e) => setSenha(e.target.value)}
+
+                   />
                 </div>
                 <div>
                     <input type="submit" value="Registrar" />

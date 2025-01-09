c0130febbfd91c117177e5ebb3011f0a6eb4a379
Adelson Saguate <adelsonsaguate@acsun-mz.com>
2025-01-09 16:23:15 +0200

    Renderiza,~ao condicional usando if aoenas

---
+++ src/components/Condicional.js
@@ -0,0 +1,47 @@
+import { useState } from "react";
+
+
+function Condicional (){
+
+    const [email, setEmail] = useState();
+    const [userEmail, setUserEmail] = useState();
+
+    function enviarEmail(e){
+       e.preventDefault();
+       setUserEmail(email)
+       console.log(userEmail);
+    
+    }
+    function limparEmail(e){
+       e.preventDefault();
+       setUserEmail('')
+
+    }
+
+    return(
+        <div>
+            <h2>Registre o seu e-mail</h2>
+            <form>
+                <input 
+                    type="email" 
+                    placeholder="Digite o seu email..."
+                    onChange={(e) => setEmail(e.target.value)}
+                />
+                <button type="submit" onClick={enviarEmail}>
+                    Enviar e-mail
+                </button>
+                {userEmail &&
+                 <div>
+                    <p>O e-mail do utilizador é: {userEmail}</p>
+                    <button onClick={limparEmail}>Limpar e-mail</button>
+                 </div>
+                
+                }
+            </form>
+        </div>
+    )
+
+
+}
+
+export default Condicional;
